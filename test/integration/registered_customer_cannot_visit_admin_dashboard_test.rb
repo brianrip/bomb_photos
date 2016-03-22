81b067f2cbe785c4613492f101108f75d726b94e
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-21 21:03:59 -0600

    Add integration test for logged in user visiting business admin dashboard

---
+++ test/integration/registered_customer_cannot_visit_admin_dashboard_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class RegisteredCustomerCannotVisitAdminDashboardTest < ActionDispatch::IntegrationTest
+  test "logged in user sees 404 page when visiting businessadmin dashboard" do
+    user = User.create(email: "adrienne@example.com", password: "password", role: 0)
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    visit '/businessadmin/dashboard'
+    assert page.has_content?("The page you were looking for doesn't exist")
+  end
+end
