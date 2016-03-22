ad549531aaefbbec32232f6f2bc9f4c430083133
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-22 10:24:41 -0600

    Add seeds file
    Draft seeds file based on initial db concept

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,69 @@
-# 10.times do
-#   Tag.create(name: Faker::Hipster.word)
-# end
-# #
-# 71.times do
-#   gif = Gif.create(title: Faker::Lorem.sentence,
-#               description: " adfg",
-#               price: 100,
-#               image: "https://placeholdit.imgix.net/~text?txtsize=60&bg=000000&txt=640%C3%97480&w=640&h=480&fm=png"
-#               )
-#   gif.tags << Tag.all.shuffle.first
-# end
-#
-#
-# 20.times do
-#   subtotal = Random.new.rand(1..10)
-#   user = User.create(username: "string", password: "password")
-#   gif = Gif.all.shuffle.first
-#   order = user.orders.create!(total_price: 3*subtotal)
-#   order.order_gifs.create!(
-#     gif_id: gif.id, quantity: 1, subtotal: subtotal
-#   )
-#   gif = Gif.all.shuffle.first
-#   order.order_gifs.create!(
-#     gif_id: gif.id, quantity: 2, subtotal: subtotal*2
-#   )
-# end
+class Seeds
 
+  def initialize
+    create_users
+    create_studio_admin
+    create_platform_admin
+    create_categories
+    create_studios
+    create_active_photos
+    @num_users = 10
+    @num_categories = 10
+    @num_studios = 3
+    @num_photos = 1000
+    @num_orders = 100
+  end
 
-Charity.create(name: "Colorado Coalition Against Sexual Assault", description: "The Colorado Coalition Against Sexual Assault (CCASA) is a membership organization promoting safety, justice and healing for survivors while working toward the elimination of sexual violence.", logo: "http://www.ccasa.org/wp-content/uploads/2015/02/logo2.png")
-Charity.create(name: "Phoenix Multisport", description: 'Phoenix Multisport fosters a supportive, physically active community for individuals who are recovering from alcohol and substance abuse and those who choose to live a sober life. Through pursuits such as climbing, hiking, running, strength training, yoga, road/mountain biking, socials and other activities, we seek to help our members develop and maintain the emotional strength they need to stay sober.', logo: "https://www.phoenixmultisport.org/images/pms_logo.png")
-Charity.create(name: "Deworm the World Initiative", description: 'Part of Evidence Action, Deworm the World Initiative is ranked as a top charity at GiveWell. A large body of evidence shows that eradicating intestinal worms in children is one of the potential biggest vectors for change in strugging communities. "Intestinal worms are debilitating, widespread, and under-treated. School-based deworming is safe, cost-effective and scale-able."', logo: "https://give.evidenceaction.org/uploads/4/3/9/6/43962849/1448300461.png")
+  def create_users
+    @num_users.times do
+      User.create(email: Faker::Internet.email, password: "password")
+    end
+  end
+
+  def create_studio_admin
+    User.create(email: Faker::Internet.email, password: "password", role: 1)
+  end
+
+  def create_platform_admin
+    User.create(email: Faker::Internet.email, password: "password", role: 2)
+  end
+
+  def create_categories
+    @num_categories.times do
+      Category.create(name: Faker::Commerce.department)
+    end
+  end
+
+  def create_studios
+    @num_studios.times do
+      Studio.create(name: Faker::Company.name)
+    end
+  end
+
+  def create_active_photos
+    @num_photos.times do
+      Photo.create(name: Faker::Commerce.product_name,
+                   description: Faker::Lorem.paragraph,
+                   category: Category.find(Random.new.rand(1..@num_categories)),
+                   price: Random.rand(2000..100000),
+                   studio: Studio.find(Random.new.rand(1..@num_studios),
+                   image: Faker::Avatar.image,
+                   status: 0)
+    end
+  end
+
+  def create_orders
+    @num_orders.times do
+      user = User.find(Random.new.rand(1..@num_users))
+      order = user.orders.create!
+      add_photos_to_order(order)
+    end
+  end
+
+  def add_photos_to_order(order)
+    10.times do
+      photo = Photo.find(Random.new.rand(1..@num_photos))
+      order.photos << photo
+    end
+  end
+end
