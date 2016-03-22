b24da3ddc0a7604b66fdfd2683023ed44f388a1f
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-22 14:49:58 -0600

    Verify that guest cannot see admin dashboard
    This functionality was in the previous codebase. Integration test verifies that it still works.

--- test/integration/user_cannot_visit_business_admin_dashboard_test.rb
+++ test/integration/user_cannot_visit_business_admin_dashboard_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserCannotVisitBusinessAdminDashboardTest < ActionDispatch::IntegrationTest
   test "user sees 404 page when trying to access business admin dashboard" do
-    visit '/buisinessadmin/dashboard'
+    visit admin_dashboard_path
     assert page.has_content?("The page you were looking for doesn't exist")
   end
 end
