519791f9c6e69e56fe4dc0f351518eb917dd9447
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-22 13:56:49 -0600

    Implement User Registering test
    Migration to change username to email
    Add additional links to user dashboard
    Added path to edit user

--- test/integration/visitor_registers_for_account_test.rb
+++ test/integration/visitor_registers_for_account_test.rb
@@ -4,15 +4,15 @@ class VisitorRegistersForAccountTest < ActionDispatch::IntegrationTest
   test "visitor registers for an account" do
     visit '/'
     click_on "Sign Up"
-    fill_in "email", with: "adrienne@example.com"
-    fill_in "password", with: "password"
-    fill_in "password confirmation", with: "password"
+    fill_in "Email", with: "adrienne@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_on "Register"
 
+    assert_equal dashboard_path, current_path
     assert page.has_content?("Thank you for creating an account!")
     assert page.has_content?("adrienne@example.com")
     assert page.has_content?("Edit Account Information")
     assert page.has_content?("View All Orders")
-    assert page.has_content?("View All Photos")
   end
 end
