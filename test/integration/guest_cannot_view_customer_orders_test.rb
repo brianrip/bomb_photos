ba6e99b061aa10f918ad7331c3f99dbc878806b2
Brian Rippeto <rippeto@gmail.com>
2016-03-21 19:44:38 -0600

    Updated test name-ready for implementation

--- test/integration/guest_cannot_view_customer_orders_test.rb
+++ test/integration/guest_cannot_view_customer_orders_test.rb
@@ -1,13 +1,16 @@
 require 'test_helper'
 
 class GuestCannotViewCustomerOrdersTest < ActionDispatch::IntegrationTest
-  test "attempt to see orders renders error page" do
+  test "unauthorized attempt to see order renders error page" do
     user = User.create(username: "Brock", password: "password",
                        password_confirmation: "password")
 
     photo = Photo.create(name: "dog in water", image_file_name: "test/asset_tests/photos/dog-swimming.jpeg", price: 200, description: "A happy labrador swims in a beautiful river.")
+
     UserPhoto.create(photo_id: photo.id)
+
     order = user.orders.create(total: 200, status: 0)
+
     visit root_path
     visit order_path(order.id)
 
