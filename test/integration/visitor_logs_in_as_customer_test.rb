5130b1fc7832ca784a60ccab6b698f530af6f20e
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-22 14:10:05 -0600

    Implement user loggin in functionality
    Change references from username to reference email
    Change styling of header links

--- test/integration/visitor_logs_in_as_customer_test.rb
+++ test/integration/visitor_logs_in_as_customer_test.rb
@@ -1,21 +1,9 @@
-# As a registered customer,
-# when I visit root,
-# I should not see "log out" on header,
-# and I should not see my username/email on header,
-# and I should see "log in" on header,
-# and when I click "log in",
-# and I fill in my username/email,
-# and I click "log in",
-# then I should see my dashboard,
-# and I should see "log out" on my header,
-# and I should not see "log in" on my header,
-# and I should see my username on my header.
 require 'test_helper'
 
 class VisitorLogsInAsCustomer < ActionDispatch::IntegrationTest
 
   test "visitor logs in" do
-    user = User.create(email: "adrienne@example.com", password: password, role: 0)
+    user = User.create(email: "adrienne@example.com", password: "password", role: 0)
     visit '/'
     refute page.has_content?("Log Out")
     refute page.has_content?(user.email)
@@ -23,10 +11,12 @@ class VisitorLogsInAsCustomer < ActionDispatch::IntegrationTest
 
     click_on "Log In"
 
-    fill_in "email", with: user.email
-    fill_in "password", with: "password"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
 
-    click_on "Log In"
+    within(".login") do
+      click_on "Log In"
+    end
 
     assert_current_path('/dashboard')
 
