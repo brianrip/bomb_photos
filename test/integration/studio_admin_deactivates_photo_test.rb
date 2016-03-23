9eaf0ce9ec35816afeae299d3546b99188670c0d
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-22 18:39:00 -0600

    Admin can deactivate a photo
    Create change status action within admin photos controller
    Delete active method within photo model
    Rename all gif to photo
    Created route for changing status
    Add activate and deactivate links to photo show view

--- test/integration/studio_admin_deactivates_photo_test.rb
+++ test/integration/studio_admin_deactivates_photo_test.rb
@@ -6,34 +6,37 @@ class StudioAdminDeactivatesPhotoTest < ActionDispatch::IntegrationTest
 
     studio = Studio.create(name:        "Studio",
                            description: "Example description.",
-                           status:      "Approved"
+                           status:      0
     )
 
-    admin = studio.users.create(username:  "admin",
-                                 password: "password",
-                                 role:     1
+    admin = studio.users.create(email:  "admin@eample.com",
+                                password: "password",
+                                role:     1
     )
 
     photo = studio.photos.create(name:        "Example Name",
                                  description: "Example Description",
-                                 image_url:   ####,
-                                 price:       999
+                                 image:       "https://placeholdit.imgix.net/~text?txtsize=60&bg=000000&txt=640%C3%97480&w=640&h=480&fm=png",
+                                 price:       999,
+                                 category_id: category.id
     )
 
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
-
-    visit admin_studio_photo_path(photo)
+    visit photo_path(photo)
+    assert page.has_content?("Add to Cart")
     click_on "Deactivate"
 
-    assert_template "admin/photos#index"
+    assert_equal photo_path(photo), current_path
     assert page.has_content?("Photo Has Been Deactivated")
+    refute page.has_content?("Add to Cart")
 
-    within(".photo")
-      assert page.has_content?("Deactivated")
+    within(".photo-show") do
+      assert page.has_content?("Activate")
+      refute page.has_content?("Deactivate")
     end
 
-    visit photos_path(category)
+    visit category_path(category.name)
 
-    refute page.has_content?(#####)
+    refute page.has_content?(photo.name)
   end
 end
