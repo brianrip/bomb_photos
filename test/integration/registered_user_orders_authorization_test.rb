4de1dafaa708fe7d7f1c32806bb5c1232c5dfd72
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-23 07:00:08 -0600

    User can only view their orders
    Updated test
    Fixed error in previous implementation

--- test/integration/registered_user_orders_authorization_test.rb
+++ test/integration/registered_user_orders_authorization_test.rb
@@ -2,17 +2,33 @@ require 'test_helper'
 
 class RegisteredUSerOrdersAuthorizationTest < ActionDispatch::IntegrationTest
   test "user who tries to view other user's order cannot" do
-    business = Business.create(name: "Nature business", description: "description", status: 0)
-    category = Category.create(name: "Nature")
-    #NEEDS REAL IMAGE STUFF
-    photo = category.photos.create(name: "name", description: "description", price: 2000, status: 0, image: "placeholder")
-    business.photos << photo
-    user = User.create(email: "adrienne@example.com", password: "password")
-    other_user = User.create(email: "brian@example.com", password: "password")
-    order = other_user.orders.create(total: 2000)
-    OrderPhoto.create(photo_id: photo.id, order_id: order.id)
-    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    category = Category.create(name: "Example Category")
 
+    studio = Studio.create(name:        "Studio",
+                           description: "Example description.",
+                           status:      0
+    )
+
+    user = studio.users.create(email:  "user@example.com",
+                                password: "password",
+                                role:     0
+    )
+    other_user = studio.users.create(email:  "other_user@example.com",
+                                password: "password",
+                                role:     0
+    )
+
+    photo = studio.photos.create(name:        "Example Name",
+                                 description: "Example Description",
+                                 image:       "https://placeholdit.imgix.net/~text?txtsize=60&bg=000000&txt=640%C3%97480&w=640&h=480&fm=png",
+                                 price:       999,
+                                 category_id: category.id
+    )
+
+    op = OrderPhoto.create(photo_id: photo.id)
+    order = other_user.orders.create(total_price: 999)
+    order.order_photos << op
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
     visit order_path(order)
     assert page.has_content?("The page you were looking for doesn't exist")
   end
