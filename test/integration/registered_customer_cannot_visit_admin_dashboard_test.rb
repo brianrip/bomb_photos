30a6fd010f47ec8f84096c0afe41d848ebd8adf9
Scott Firestone <scott.firestone.dev@gmail.com>
2016-03-23 15:08:04 -0600

    Test registered user cannot access admin dashboard
    Functionality existed, needed to change references from  username to email

--- test/integration/registered_customer_cannot_visit_admin_dashboard_test.rb
+++ test/integration/registered_customer_cannot_visit_admin_dashboard_test.rb
@@ -4,7 +4,9 @@ class RegisteredCustomerCannotVisitAdminDashboardTest < ActionDispatch::Integrat
   test "logged in user sees 404 page when visiting businessadmin dashboard" do
     user = User.create(email: "adrienne@example.com", password: "password", role: 0)
     ApplicationController.any_instance.stubs(:current_user).returns(user)
-    visit '/businessadmin/dashboard'
+
+    visit admin_dashboard_path
+
     assert page.has_content?("The page you were looking for doesn't exist")
   end
 end
