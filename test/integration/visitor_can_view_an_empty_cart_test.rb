7f5cce8b25506d7e2593b43de255b0fbd50739ff
Brian Rippeto <rippeto@gmail.com>
2016-03-22 20:12:15 -0600

    Write integration test for visitor can view an empty cart

--- test/integration/visitor_can_view_an_empty_cart_test.rb
+++ test/integration/visitor_can_view_an_empty_cart_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class VisitorCanViewAnEmptyCartTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "visitor sees an empty cart" do
+    visit root_path
+    click_on "Cart"
+    assert page.has_content? "Your cart is empty"
+  end
 end
