909ca7188950acb441c5d86642b509dfca98038f
Brian Rippeto <rippeto@gmail.com>
2016-03-22 20:06:57 -0600

    Write feature test for visitor can add photos to cart

--- test/integration/visitor_can_add_photos_to_cart_test.rb
+++ test/integration/visitor_can_add_photos_to_cart_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class VisitorCanAddPhotosToCartTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "visitor sees a cart with multiple items"
+
+    category = Category.create(name: "Example Category")
+    Category.create(name: "Other Category")
+
+    studio = Studio.create(name:        "Studio",
+                           description: "Example description.",
+                           status:      0
+    )
+
+    photo = studio.photos.create(name:        "Example Name",
+                                 description: "Example Description",
+                                 image:       "https://placeholdit.imgix.net/~text?txtsize=60&bg=000000&txt=640%C3%97480&w=640&h=480&fm=png",
+                                 price:       999,
+                                 category_id: category.id
+    )
+
+    photo2 = studio.photos.create(name:        "Photo 2",
+                                 description: "Example Description 2",
+                                 image:       "https://placeholdit.imgix.net/~text?txtsize=60&bg=000000&txt=640%C3%97480&w=640&h=480&fm=png",
+                                 price:       999,
+                                 category_id: category.id
+    )
+
+    visit categories_path
+
+    click_on category.name
+    click_on photo.name
+    click_on "Add to Cart"
+
+    assert page.has_content? "Photo has been added to cart"
+    assert page.has_content? "cart(1)"
+
+    visit category_path(category.name)
+
+    click_on photo2.name
+    click_on "Add to Cart"
+
+    assert page.has_content? "Photo has been added to cart"
+    assert page.has_content? "cart(2)"
+  end
 end
