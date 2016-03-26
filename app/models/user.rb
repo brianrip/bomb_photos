73f2bcf200abec432949a631778733df179f4b36
Scott <scottfirestone@users.noreply.github.com>
2016-03-26 16:05:33 -0600

    Update user.rb
    Clean ruby style, remove extra line.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, :format => { :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
-
   validates :password, presence: true
 
   has_many :orders
