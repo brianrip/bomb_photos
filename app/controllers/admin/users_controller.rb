804f60198f1d9a0ab8ff48ea07bb5b1b634d0b69
Brian Rippeto <rippeto@gmail.com>
2016-03-26 22:17:47 -0600

    Studio admin can add or remove other admin
    Create admin user index
    Add update studio admin action
    Implement new methods to check user role

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,25 +4,25 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def index
-    require "pry"
-    binding.pry
     @users = User.all
   end
 
   def change_admin_status
     @user = User.find(params[:id])
-    if @user.studio_admin?
+    if @user == current_user
+      flash[:error] = "You cannot remove yourself at this time, please contact Bomb Photos to perform this action."
+    elsif @user.studio_admin?
       @user.update_attribute(:studio_id, nil)
       @user.user_roles.each do |user_role|
         if user_role.role.name == "studio admin"
           @user.user_roles.find(user_role.id).delete
         end
       end
-      flash[:success] = "User's admin privileges have been revoked"
+      flash[:success] = "You have removed admin status for #{@user.email}"
     else
       @user.roles << Role.find_or_create_by(name: "studio admin")
       @user.update_attribute(:studio_id, current_user.studio.id)
-      flash[:success] = "User has been granted admin status!"
+      flash[:success] = "#{@user.email} has been granted admin status!"
     end
     redirect_to :back
   end
--- test/integration/studio_admin_can_add_or_remove_user_test.rb
+++ test/integration/studio_admin_can_add_or_remove_user_test.rb
@@ -17,7 +17,31 @@ class StudioAdminCanAddOrRemoveUserTest < ActionDispatch::IntegrationTest
     assert page.has_content? "#{user.email} has been granted admin status!"
 
     assert page.has_link? "Revoke admin status for user #{admin.id}"
-save_and_open_page
     refute page.has_link? "Grant admin status for user #{user.id}"
+  end
+
+  test "studio admin removes a fellow studio admin but not self" do
+    category = create_category
+    studio = create_studio
+    admin = create_studio_admin(studio)
+    admin2 = create_studio_admin(studio)
+
+    user = create_user
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_path(studio)
+    click_on "Manage studio administrative privileges"
+
+    assert page.has_link? "Revoke admin status for user #{admin2.id}"
+    assert page.has_link? "Grant admin status for user #{user.id}"
+
+    click_on "Revoke admin status for user #{admin.id}"
+    assert page.has_content?"You cannot remove yourself at this time, please contact Bomb Photos to perform this action."
+
+    click_on "Revoke admin status for user #{admin2.id}"
+    assert page.has_content? "You have removed admin status for #{admin2.email}"
+    assert page.has_link? "Grant admin status for user #{user.id}"
+    assert page.has_link? "Grant admin status for user #{admin2.id}"
+    refute page.has_link? "Revoke admin status for user #{admin2.id}"
   end
 end
