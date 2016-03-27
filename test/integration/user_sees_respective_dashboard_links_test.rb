2e50227140dd6dd0ecef0f94360f27a15d5abad7
Brian Rippeto <rippeto@gmail.com>
2016-03-27 16:46:11 -0600

    Add studio name to dashboard

--- test/integration/user_sees_respective_dashboard_links_test.rb
+++ test/integration/user_sees_respective_dashboard_links_test.rb
@@ -4,13 +4,13 @@ class UserSeesRespectiveDashboardLinksTest < ActionDispatch::IntegrationTest
   test "platform studio admin customer sees three account links in nav " do
     studio = create_studio
     create_and_login_studio_platform_admin(studio)
-
     visit root_path
 
     assert page.has_content? "Customer account"
-    assert page.has_content? "Studio admin account"
+    assert page.has_content? "#{studio.name} admin account"
     assert page.has_content? "Platform admin account"
   end
+
   test "studio admin customer sees two account links in nav " do
     studio = create_studio
     create_and_login_studio_platform_admin(studio)
@@ -18,8 +18,9 @@ class UserSeesRespectiveDashboardLinksTest < ActionDispatch::IntegrationTest
     visit root_path
 
     assert page.has_content? "Customer account"
-    assert page.has_content? "Studio admin account"
+    assert page.has_content? "#{studio.name} admin account"
   end
+
   test "customer sees one account link in nav " do
     studio = create_studio
     create_and_login_studio_platform_admin(studio)
