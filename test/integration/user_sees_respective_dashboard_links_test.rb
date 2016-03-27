452e2058d2b44b7cc594ce81c4a9cf3ad3d6c14a
Brian Rippeto <rippeto@gmail.com>
2016-03-27 16:26:03 -0600

    User can access all authorized accounts in navbar
    Change font of navbar
    Add condtions in header partial to display proper account links
    Remove cart glypicon

---
+++ test/integration/user_sees_respective_dashboard_links_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UserSeesRespectiveDashboardLinksTest < ActionDispatch::IntegrationTest
+  test "platform studio admin customer sees three account links in nav " do
+    studio = create_studio
+    create_and_login_studio_platform_admin(studio)
+
+    visit root_path
+
+    assert page.has_content? "Customer account"
+    assert page.has_content? "Studio admin account"
+    assert page.has_content? "Platform admin account"
+  end
+  test "studio admin customer sees two account links in nav " do
+    studio = create_studio
+    create_and_login_studio_platform_admin(studio)
+
+    visit root_path
+
+    assert page.has_content? "Customer account"
+    assert page.has_content? "Studio admin account"
+  end
+  test "customer sees one account link in nav " do
+    studio = create_studio
+    create_and_login_studio_platform_admin(studio)
+
+    visit root_path
+
+    assert page.has_content? "Customer account"
+  end
+end
